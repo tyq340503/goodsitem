506cd949d6e2ddc8c4be359c37b0ec936a762575
tyq <1135409448@qq.com>
2018-10-13 22:37:00 -0400

    add stripe

--- models/user.js
+++ models/user.js
@@ -1,9 +1,12 @@
 var mongoose = require("mongoose");
+var bcrypt = require('bcrypt-nodejs');
+var crypto = require('crypto');
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String,
+    email: { type: String, unique: true, lowercase: true}
 });
 
 UserSchema.plugin(passportLocalMongoose)
