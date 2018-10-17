0bcdb855a75cea1e47dabd6e69064f1077e509e1
tyq <1135409448@qq.com>
2018-10-16 23:16:23 -0400

    test

--- routes/index.js
+++ routes/index.js
@@ -128,5 +128,28 @@ router.post('/add-category', function (req, res, next) {
     });
 })
 
+router.get('/products/:id', function (req, res, next) {
+    Product
+        .find({ category: req.params.id })
+        .populate('category')
+        .exec(function (err, products) {
+            if (err) return next(err);
+            res.render('main/category', {
+                products: products
+            });
+        });
+});
+
+router.get('/product/:id', function (req, res, next) {
+    Product.findById({ _id: req.params.id }, function (err, product) {
+        if (err) return next(err);
+        res.render('main/product', {
+            product: product
+        });
+    });
+});
+
+
+
 module.exports = router;
 
